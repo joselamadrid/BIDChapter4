7c133ddee1dcc1a2480a4cc5acfb71297e48cd0b
Joselamadrid <sysadminktp@mail.ru>
2013-02-15 15:00:42 +0200

    Initial commit

---
+++ Control fun/BIDViewController.h
@@ -0,0 +1,13 @@
+//
+//  BIDViewController.h
+//  Control fun
+//
+//  Created by Josemac on 15.02.13.
+//  Copyright (c) 2013 Jose. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDViewController : UIViewController
+
+@end
---
+++ Control fun/BIDViewController.m
@@ -0,0 +1,29 @@
+//
+//  BIDViewController.m
+//  Control fun
+//
+//  Created by Josemac on 15.02.13.
+//  Copyright (c) 2013 Jose. All rights reserved.
+//
+
+#import "BIDViewController.h"
+
+@interface BIDViewController ()
+
+@end
+
+@implementation BIDViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
